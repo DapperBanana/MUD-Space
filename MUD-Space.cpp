06b6a730753f9422161539b202e0b52c9fe4c5f3
Austin Howard <contact@austinlhoward.com>
2026-03-16 22:12:00 -0600

    Wire up server in main and update CMakeLists for Boost

--- MUD-Space.cpp
+++ MUD-Space.cpp
@@ -1,12 +1,27 @@
-﻿// MUD-Space.cpp : Defines the entry point for the application.
-//
-
 #include "MUD-Space.h"
+#include "Server.h"
+
+#include <iostream>
+#include <exception>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	const unsigned short port = 4000;
+
+	try {
+		boost::asio::io_context io_context;
+		Server server(io_context, port);
+
+		cout << "MUD-Space starting on port " << port << endl;
+		server.start_accept();
+		io_context.run();
+	}
+	catch (const exception& e) {
+		cerr << "Fatal: " << e.what() << endl;
+		return 1;
+	}
+
 	return 0;
 }
