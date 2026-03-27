e3dd6ace366dd1b53dfd3a7176f196831413349e
Austin L. Howard <contact@austinlhoward.com>
2026-03-27 17:14:42 -0600

    Fix: Echo input back to client with newline

--- src/Session.cpp
+++ src/Session.cpp
@@ -9,8 +9,8 @@ Session::Session(std::shared_ptr<tcp::socket> socket)
 
 void Session::start()
 {
-    do_read();
     do_write("Welcome to MUD-Space!\r\n> ");
+    do_read();
 }
 
 void Session::do_read()
@@ -19,16 +19,15 @@ void Session::do_read()
     socket_->async_read_some(
         boost::asio::buffer(read_buffer_),
         [this, self](const boost::system::error_code& ec, std::size_t length) {
-            if (!ec) {
-                std::string input(read_buffer_, length);
-                std::cout << "Received: " << input << std::endl;
-
-                // Echo back the input
-                do_write("You typed: " + input + "\r\n> ");
-                do_read(); // Continue reading
-            } else {
-                std::cerr << "Client disconnected: " << ec.message() << std::endl;
+            if (ec) {
+                std::cout << "Client disconnected" << std::endl;
+                return;
             }
+
+            std::string input(read_buffer_, length);
+            std::cout << "Received: " << input << std::endl;
+            do_write(input + "\r\n> ");
+            do_read();
         });
 }
 
@@ -40,7 +39,7 @@ void Session::do_write(const std::string& msg)
         boost::asio::buffer(msg),
         [this, self](boost::system::error_code ec, std::size_t /*length*/) {
             if (ec) {
-                std::cerr << "Write error: " << ec.message() << std::endl;
+                std::cout << "Error writing to client" << std::endl;
             }
         });
 }
