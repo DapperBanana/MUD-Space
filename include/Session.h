c473acba5c05e70628827552d5c56c25f2201bd3
Austin L. Howard <contact@austinlhoward.com>
2026-04-05 22:02:00 -0600

    Add: Player struct for session state

--- include/Session.h
+++ include/Session.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <boost/asio.hpp>
-#include <memory>
 #include <string>
-#include "CommandParser.h"
+#include <memory>
 #include "CommandDispatcher.h"
 
 using boost::asio::ip::tcp;
 
-class Session : public std::enable_shared_from_this<Session> {
+struct Player
+{
+    std::string name = "Nameless";
+};
+
+class Session : public std::enable_shared_from_this<Session>
+{
 public:
-    explicit Session(std::shared_ptr<tcp::socket> socket);
+    Session(std::shared_ptr<tcp::socket> socket);
     void start();
     void disconnect();
 
@@ -19,8 +24,9 @@ private:
     void do_write(const std::string& msg);
 
     std::shared_ptr<tcp::socket> socket_;
-    char read_buffer_[1024];
-    CommandParser parser_;
-    CommandDispatcher dispatcher_;
+    boost::asio::streambuf read_buffer_;
     bool disconnecting_ = false;
+    CommandDispatcher command_dispatcher_;
+
+    Player player_;
 };
