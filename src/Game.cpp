a8ccd8baeccc5b3936b285c91d27afbf4f9b7455
Austin L. Howard <contact@austinlhoward.com>
2026-04-07 21:46:00 -0600

    Update: Game loop add new player

--- src/Game.cpp
+++ src/Game.cpp
@@ -13,6 +13,10 @@ void Game::run()
 
     while (running_)
     {
+        //Add new player to the game
+        Player newPlayer = {"Test"};
+        players_[players_.size()+1] = newPlayer;
+
         std::cout << "Game tick" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
