7a66b5c07f5d50ce84483a7a213abb1578b99ff9
Austin L. Howard <contact@austinlhoward.com>
2026-05-11 20:54:00 -0600

    Update: Room 'look' command includes item descriptions

--- src/CommandDispatcher.cpp
+++ src/CommandDispatcher.cpp
@@ -13,53 +13,10 @@ void CommandDispatcher::register_defaults()
         if (session.player_.currentRoom != nullptr) {
             std::ostringstream oss;
             oss << session.player_.currentRoom->description << "\r\n";
+            oss << session.player_.currentRoom->describe_items();
             return oss.str();
         } else {
             return "You are floating in an empty void. Stars glimmer faintly in every direction.\r\n";
-        }
-    };
-
-    handlers_["north"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
-        if (session.player_.currentRoom != nullptr && session.player_.currentRoom->exits.count("north")) {
-            session.player_.currentRoom = session.player_.currentRoom->exits["north"];
-            std::ostringstream oss;
-            oss << session.player_.currentRoom->description << "\r\n";
-            return oss.str();
-        } else {
-            return "You cannot go that way.\r\n";
-        }
-    };
-
-    handlers_["south"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
-        if (session.player_.currentRoom != nullptr && session.player_.currentRoom->exits.count("south")) {
-            session.player_.currentRoom = session.player_.currentRoom->exits["south"];
-            std::ostringstream oss;
-            oss << session.player_.currentRoom->description << "\r\n";
-            return oss.str();
-        } else {
-            return "You cannot go that way.\r\n";
-        }
-    };
-
-    handlers_["east"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
-        if (session.player_.currentRoom != nullptr && session.player_.currentRoom->exits.count("east")) {
-            session.player_.currentRoom = session.player_.currentRoom->exits["east"];
-            std::ostringstream oss;
-            oss << session.player_.currentRoom->description << "\r\n";
-            return oss.str();
-        } else {
-            return "You cannot go that way.\r\n";
-        }
-    };
-
-    handlers_["west"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
-        if (session.player_.currentRoom != nullptr && session.player_.currentRoom->exits.count("west")) {
-            session.player_.currentRoom = session.player_.currentRoom->exits["west"];
-            std::ostringstream oss;
-            oss << session.player_.currentRoom->description << "\r\n";
-            return oss.str();
-        } else {
-            return "You cannot go that way.\r\n";
         }
     };
 }
