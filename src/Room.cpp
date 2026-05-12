05b97f9cf68704f8953ded1a68e555278306d534
Austin L. Howard <contact@austinlhoward.com>
2026-05-11 20:10:00 -0600

    Add: Items to rooms with basic representation

--- src/Room.cpp
+++ src/Room.cpp
@@ -1,4 +1,18 @@
 #include "Room.h"
+#include <sstream>
 
 Room::Room(const std::string& name, const std::string& description)
     : name(name), description(description) {}
+
+std::string Room::describe_items() const {
+    if (items.empty()) {
+        return "There are no items here.\r\n";
+    }
+
+    std::ostringstream oss;
+    oss << "Items here:\r\n";
+    for (const auto& item : items) {
+        oss << "- " << item.name << ": " << item.description << "\r\n";
+    }
+    return oss.str();
+}
--- src/Room.h
+++ src/Room.h
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <string>
-#include <map>
+#include <vector>
+
+class Item {
+public:
+    std::string name;
+    std::string description;
+
+    Item(const std::string& name, const std::string& description) : name(name), description(description) {}
+};
 
 class Room {
 public:
     std::string name;
     std::string description;
-    std::map<std::string, Room*> exits; // Direction -> Room*
+    std::vector<Item> items;
 
     Room(const std::string& name, const std::string& description);
+    std::string describe_items() const;
 };
