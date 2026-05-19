52bdd67cfc15fff079d39192049379337a12a243
Austin L. Howard <contact@austinlhoward.com>
2026-05-18 19:45:00 -0600

    Fix: Typo in CommandDispatcher look command handling

--- src/CommandDispatcher.cpp
+++ src/CommandDispatcher.cpp
@@ -16,29 +16,36 @@ void CommandDispatcher::register_defaults()
             oss << session.player_.currentRoom->describe_items();
             return oss.str();
         } else {
-            return "You are floating in an empty void. Stars glimmer faintly in every direction.\r\n";
+            return "You are floating in an empty void.\r\n";
         }
     };
-
+    handlers_["north"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
+        return session.handle_move("north");
+    };
+    handlers_["south"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
+        return session.handle_move("south");
+    };
+    handlers_["east"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
+        return session.handle_move("east");
+    };
+    handlers_["west"] = [](Session& session, const ParsedCommand& /*cmd*/) -> std::string {
+        return session.handle_move("west");
+    };
     handlers_["get"] = [](Session& session, const ParsedCommand& cmd) -> std::string {
-        if (cmd.args.empty()) {
-            return "Get what?\r\n";
-        }
-
-        std::string itemName = cmd.args[0];
-        Room* room = session.player_.currentRoom;
-        if (room == nullptr) {
-            return "You are floating in the void. There is nothing to get.\r\n";
-        }
-
-        for (size_t i = 0; i < room->items.size(); ++i) {
-            if (room->items[i].name == itemName) {
-                // Item found! Remove it from the room and add it to player inventory (not implemented yet)
-                room->items.erase(room->items.begin() + i);
-                return "You got the " + itemName + ".\r\n";
-            }
-        }
-
-        return "There is no " + itemName + " here.\r\n";
+        return session.handle_get(cmd);
     };
+
+}
+
+CommandHandler CommandDispatcher::get_handler(const std::string& verb) {
+    auto it = handlers_.find(verb);
+    if (it != handlers_.end()) {
+        return it->second;
+    } else {
+        return default_handler_;
+    }
+}
+
+std::string CommandDispatcher::default_handler_(Session& /*session*/, const ParsedCommand& /*cmd*/) {
+    return "Invalid command.\r\n";
 }
